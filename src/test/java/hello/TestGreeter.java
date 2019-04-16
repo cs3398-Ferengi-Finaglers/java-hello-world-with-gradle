aaade700f5d5518be718847ddc09ae4720bb3a92
KProft <kcp36@txstate.edu>
2019-04-16 02:57:52 -0500

    added failing tests

--- src/test/java/hello/TestGreeter.java
+++ src/test/java/hello/TestGreeter.java
@@ -67,7 +67,7 @@ public class TestGreeter {
    // These two tests should pass
    @Test
 
-   public void testGreeterKaren() 
+   public void testGreeterKarenPass() 
 
    {
        g.setName("Karen");
@@ -85,6 +85,21 @@ public class TestGreeter {
 
    {
        g.setName(Integer.toString(123));
+
+      assertEquals(g.getName(),"123");
+
+      assertEquals(g.sayHello(),"Hello 123!");
+
+   }
+   
+   // This test should fail
+   
+   @Test
+
+   public void newtestKcpGreeterFail() 
+
+   {
+       g.setName(123);
 
       assertEquals(g.getName(),"123");
 
